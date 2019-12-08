4d65f4b58a8b1218ab2d55df1bd76beb4701e21c
Alex Shortt <alexander.shortt@gmail.com>
2019-12-08 02:05:35 -0800

    add ambient reactivity to entity

--- src/clips/intro.js
+++ src/clips/intro.js
@@ -5,6 +5,7 @@ import Entity from "components/Entity"
 let LAC
 let journey
 let subject
+const entities = []
 
 export default function sketch(p) {
   // eslint-disable-next-line no-param-reassign
@@ -18,13 +19,22 @@ export default function sketch(p) {
     journey = new Journey(p)
     journey.setup()
 
-    subject = new Entity(
-      p,
-      64,
-      p.createVector(0, 0, 0),
-      p.createVector(0, 0, 0),
-      p.color(100, 100, 100)
-    )
+    // subject = new Entity(
+    //   p,
+    //   64,
+    //   p.createVector(0, 0, 0),
+    //   p.createVector(0, 0, 0),
+    //   p.color(100, 100, 100)
+    // )
+
+    for (let i = 0; i < 20; i += 1) {
+      entities.push(
+        new Entity(p, {
+          size: 64,
+          pos: p.createVector(i * 70 - 600, (i % 8) * 70 - 200, 0)
+        })
+      )
+    }
   }
 
   // eslint-disable-next-line no-param-reassign
@@ -33,15 +43,13 @@ export default function sketch(p) {
   }
 
   // eslint-disable-next-line no-param-reassign
-  p.keyPressed = () => {
-    subject.rotate(p.createVector(50, 0, 10))
-  }
-
-  // eslint-disable-next-line no-param-reassign
   p.draw = () => {
     p.background(255)
-    LAC.render(250, -200, -200)
-    journey.render(-250, 0, -400, 0, 0, 0)
-    subject.render()
+    // LAC.render(250, -200, -200)
+    // journey.render(-250, 0, -400, 0, 0, 0)
+    // subject.render()
+    for (const entity of entities) {
+      entity.render()
+    }
   }
 }
--- src/components/Entity/index.js
+++ src/components/Entity/index.js
@@ -1,11 +1,30 @@
 import p5 from "p5"
+import hash from "hash.js"
+import uuid from "uuid/v4"
 
 export default class Entity {
-  constructor(p, size, pos, rot, fill) {
+  constructor(p, props) {
+    const {
+      size,
+      pos = p5.Vector.random3D(),
+      rot = p5.Vector.random3D(),
+      fill = p.color(Math.random() * 360, 100, 60),
+      seed,
+      ambientWeight = 1
+    } = props
+
+    this.id = hash
+      .sha256()
+      .update(seed || uuid())
+      .digest("hex")
+    this.ambientWeight = ambientWeight
+
     this.p = p
 
     this.size = size
     this.targetSize = size
+
+    this.ambientDelta = 0
 
     this.pos = pos
     this.targetPos = pos.copy()
@@ -16,7 +35,10 @@ export default class Entity {
     this.fill = fill
 
     this.fill = fill
+    this.offsetFill = p.createVector(0, 0, 0)
     this.targetFill = p.color(fill)
+
+    console.log(`Entity with id ${this.id} created`)
   }
 
   update() {
@@ -56,14 +78,70 @@ export default class Entity {
     )
   }
 
+  ambientEffect() {
+    const { p, size } = this
+
+    const weights = []
+    const numWeights = 16
+    for (let i = 0; i < numWeights; i += 1) {
+      weights[i] = this.getIdVal(Math.floor((i / numWeights) * 20), 4)
+    }
+
+    // if (Math.random() > weights[0] * 0.1) {
+    //   return
+    // }
+
+    // increment noise walker
+    this.ambientDelta += weights[4]
+
+    // color mod
+    const hueShift = this.weightedNoise(weights[1], 0.005, 40)
+    const satShift = this.weightedNoise(weights[2], 0.001, 10)
+    const brightShift = this.weightedNoise(weights[3], 0.001, 10)
+    // console.log("color: ", hueShift, satShift, brightShift)
+    this.setOffsetFill(hueShift, satShift, brightShift)
+
+    // pos mod
+    const dx = this.weightedNoise(weights[5], 0.01, size * 0.0001)
+    const dy = this.weightedNoise(weights[6], 0.01, size * 0.0001)
+    const dz = this.weightedNoise(weights[7], 0.01, size * 0.0001)
+    // console.log("pos: ", dx, dy, dz)
+    this.translate(p.createVector(dx, dy, dz))
+
+    // rot mod
+    const rx = this.weightedNoise(weights[9], 0.001, size * 0.00001)
+    const ry = this.weightedNoise(weights[10], 0.001, size * 0.00001)
+    const rz = this.weightedNoise(weights[11], 0.001, size * 0.00001)
+    // console.log("rot: ", rx, ry, rz)
+    this.rotate(p.createVector(rx, ry, rz))
+  }
+
+  getIdVal(pos, length) {
+    return (
+      parseInt(this.id.substring(pos, pos + length), 16) /
+      (Math.pow(16, length) - 1)
+    )
+  }
+
+  weightedNoise(weight, speed, amplitude) {
+    const { p, ambientDelta, ambientWeight } = this
+    return (
+      (p.noise(weight, ambientDelta * speed) - 0.5) *
+      (weight * amplitude) *
+      ambientWeight
+    )
+  }
+
   render() {
+    this.ambientEffect()
+
     this.update()
 
     const { p, pos, rot, size, fill } = this
 
     p.push()
     p.strokeWeight(2)
-    p.fill(fill)
+    p.fill(this.getFillWithOffset())
     p.translate(pos)
     p.rotateX(rot.x)
     p.rotateY(rot.y)
@@ -82,5 +160,18 @@ export default class Entity {
 
   rotate(addRot) {
     this.targetRot.add(addRot)
+  }
+
+  setOffsetFill(dh, ds, db) {
+    const { p } = this
+    this.offsetFill = p.createVector(dh, ds, db)
+  }
+
+  getFillWithOffset() {
+    const { p, fill, offsetFill } = this
+    const hue = p.hue(fill) + offsetFill.x
+    const sat = p.saturation(fill) + offsetFill.y
+    const bright = p.brightness(fill) + offsetFill.z
+    return p.color(hue, sat, bright)
   }
 }
